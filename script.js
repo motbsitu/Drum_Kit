25fbd412183f1fafeed62312a13470ac78827e8d
Julie S Mike <juliesmike@gmail.com>
2017-01-27 12:36:54 -0600

    end event on transition

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-window.addEventListener('keydown', function (e){
-  console.log(event.keyCode);
-  const audio = document.querySelector('audio[data-key="${e.keyCode}"]');
-  console.log(audio);
-});
+function playSound(e){
+  // console.log(e.keyCode);
+  const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
+  const key = document.querySelector(`.key[data-key='${e.keyCode}']`);
+
+  // console.log(audio);
+  if(!audio) return;  //stop function if wrong key hit
+  audio.currentTime = 0; //rewind to start
+  audio.play();
+  // console.log('key', key);
+  key.classList.add('playing');
+};
+
+function removeTransition(e){
+  if(e.propertyName !== 'transform') return;
+  // console.log(e.propertyName);
+  this.classList.remove('playing')
+};
+
+const keys = document.querySelectorAll('.key');
+// console.log('keys', keys);
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
